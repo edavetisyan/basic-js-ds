13009776db0963ff82e469cb19a5217e8fee6ac1
edavetisyan <edmondavetisyanw@gmail.com>
2021-10-16 19:58:34 +0300

    fix: syntax fix

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,104 +6,104 @@ const { Node } = require("../extensions/list-tree.js");
  * Implement simple binary search tree according to task description
  * using Node from extensions
  */
-module.exports = class BinarySearchTree {
-  node = null
+ module.exports = class BinarySearchTree {
+
+  node=null
+
   root() {
-    return this.node;
+    return this.node
   }
 
-  add(data) {
-    this.root = addData(this.root, data);
-    function addData(node, data) {
-      if (!node) {
-        return new Node(data);
+  add(data) {   
+      this.node=addData(this.node, data)
+
+      function addData(node, data) {
+        if(!node){
+          return new Node(data)
+        }
+        if(node.data===data){
+          return node
+        } else {
+          if(data<node.data) {
+            node.left= addData(node.left, data)
+          } else {
+            node.right= addData(node.right, data)
+        }
+        return node
+        }
       }
-      if (node.data === data) {
-        return node;
-      }
-      if (data < node.data) {
-        node.left = addData(node.left, data);
-      } else {
-        node.right = addData(node.right, data);
-      }
-      return node;
-    }
   }
 
   has(data) {
-    return hasElement(this.node, data);
-
+    return hasElement(this.node,data)
     function hasElement(node, data) {
-      if (!node) {
-        return false;
+      if(!node) {
+        return false
       }
-      if (node.data === data) {
-        return true;
-      } else {
-        if (data < node.data) {
-          return hasElement(node.left, data);
+      if(node.data===data){
+        return true
+      } else{
+        if(data<node.data) {
+          return hasElement(node.left, data)
         } else {
-          return hasElement(node.right, data);
+          return hasElement(node.right, data)
         }
       }
     }
   }
 
   find(data) {
-    return findElement(this.node, data);
+    return findElement(this.node,data)
     function findElement(node, data) {
-      if (!node) {
-        return null;
+      if(!node) {
+        return null
       }
-      if (node.data === data) {
-        return node;
-      } else {
-        if (data < node.data) {
-          return findElement(node.left, data);
+      if(node.data===data){
+        return node
+      } else{
+        if(data<node.data) {
+          return findElement(node.left, data)
         } else {
-          return findElement(node.right, data);
+          return findElement(node.right, data)
         }
       }
     }
   }
 
   remove(data) {
-    this.node =removeElement(this.node, data)
+    this.node = removeElement(this.node,data)
 
-    function removeElement(node, data){
-      if (!node) {
+    function removeElement(node, data) {
+      if(!node){
         return node
-      }else{
-        if(data<node.data){
-          node.left = removeElement(node.left,data)
+      } else {
+        if(data<node.data) {
+          node.left=removeElement(node.left, data)
+          return node
+        } 
+        if(data>node.data) {
+          node.right=removeElement(node.right, data)
           return node
         }
-        if (data>node.data) {
-          node.right = removeElement(node.right, data)
-          return node
+        if(data===node.data) {
+          if(!node.left&&!node.right) {
+            return null
+          } else if(!node.left){
+            node=node.right
+            return node
+          } else if(!node.right) {
+            node=node.left
+            return node
+          } else {
+            let nodeRight=node.right
+            while(nodeRight.left){
+              nodeRight=nodeRight.left
+            }
+            node.data=nodeRight.data
+            node.right=removeElement(node.right, nodeRight.data)
+            return node
+          }
         }
-
-if (data === node.data) {
-  if(!node.left && !node.right){
-    return null
-  }else if (!node.left) {
-    node = node.right
-    return node
-  }else if (!node.right) {
-    node = node.left
-    return node
-  }
-  else{
-    let nodeRight = node.right
-    while(nodeRight.left){
-      nodeRight = nodeRight.left
-    }
-    node.data = nodeRight.data
-    node.right = removeElement(node.right, nodeRight.left)
-    return node
-  }
-}
-
       }
     }
   }
@@ -130,4 +130,4 @@ if (data === node.data) {
       return thisNode.data
   }
 
-};
+}
